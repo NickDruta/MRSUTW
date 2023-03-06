46801fbd207307ac9b804c7e3c8b618134a71ff3
nickdruta <nickdruta@NICKDRUTAA15A>
2023-03-06 20:36:39 +0200

    Add Login and Signup

---
+++ MRSUTW/Controllers/SingUpController.cs
@@ -0,0 +1,22 @@
+﻿using MRSUTW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRSUTW.Controllers
+{
+    public class SingUpController : Controller
+    {
+        // GET: SingUp
+        public ActionResult Index()
+        {
+            User u = new User();
+            u.Username = "admin";
+            u.Password = "parola";
+
+            return View(u);
+        }
+    }
+}
