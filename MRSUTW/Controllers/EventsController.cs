33d7318e4ea0a2b044e02f8e4762eaed2ecf2179
Antonio <45819634+AnLuk13@users.noreply.github.com>
2023-04-13 12:36:17 +0300

    Add event page functionality

---
+++ MRSUTW/Controllers/EventsController.cs
@@ -0,0 +1,19 @@
+﻿using MRSUTW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MRSUTW.Models;
+
+namespace MRSUTW.Controllers
+{
+    public class EventsController : Controller
+    {
+        EventContent db = new EventContent();
+        public ViewResult EventUse()
+          {
+               return View(db.list.ToList());
+          }
+    }
+}
