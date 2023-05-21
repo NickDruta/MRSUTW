f3610a1f40ef235793dcab134d5fa4e17538a8e4
Antonio <45819634+AnLuk13@users.noreply.github.com>
2023-05-21 21:56:59 +0300

    admin events

---
+++ MRSUTW/Controllers/AdminEventsController.cs
@@ -0,0 +1,38 @@
+﻿using MRSUTW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MRSUTW.Models;
+using MRSUTW.BusinessLogic.Interfaces;
+using MRSUTW.Domain.Entities.Pereche;
+using MRSUTW.Domain.Entities.Event;
+using AutoMapper;
+
+namespace MRSUTW.Controllers
+{
+     public class AdminEventsController : Controller
+     {
+          private IEvent _event;
+
+          public AdminEventsController()
+          {
+               var bl = new BusinessLogic.BusinessLogic();
+               _event = bl.GetEventBL();
+          }
+          public ActionResult Index()
+          {
+               var config = new MapperConfiguration(cfg => {
+                    cfg.CreateMap<EventData, Event>();
+               });
+
+               IMapper mapper = config.CreateMapper();
+
+               List<EventData> eventDataList = _event.GetEvents();
+               List<Event> listEvent = eventDataList.Select(eventData => mapper.Map<Event>(eventData)).ToList();
+
+               return View(listEvent);
+          }
+     }
+}
