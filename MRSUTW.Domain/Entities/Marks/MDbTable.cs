5f189bbcb8064cdda804a512d3a4e2273b05beef
Nichita <124071977+Quggos@users.noreply.github.com>
2023-05-24 16:23:02 +0300

    add Marks to Db (#56)

---
+++ MRSUTW.Domain/Entities/Marks/MDbTable.cs
@@ -0,0 +1,42 @@
+﻿using MRSUTW.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSUTW.Domain.Entities.Marks
+{
+    public class MDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string StudentEmail { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Obiect { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TeacherEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Start { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime End { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string Value { get; set; }
+    }
+}
---
+++ MRSUTW.Domain/Entities/Marks/MarksData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSUTW.Domain.Entities.Marks
+{
+    public class MarksData
+    {
+        public int Id { get; set; }
+        public string StudentEmail { get; set; }
+        public string Obiect { get; set; }
+        public string TeacherEmail { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Value { get; set; }
+    }
+}
